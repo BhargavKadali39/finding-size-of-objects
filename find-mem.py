67c28e50ec325e66b9ceaa7c847bbf1317617cb5
Bhargav <bhargavkadali39@gmail.com>
2021-10-21 21:20:13 +0530

    Update find-mem.py

--- find-mem.py
+++ find-mem.py
@@ -1,1 +1,12 @@
 
+import sys
+
+size_1 = ["I","am","groot"]
+
+size_2 = ("I","am","groot")
+
+print("size of first object:",end=" ")
+print(sys.getsizeof(size_1))
+
+print("size of second object:",end=" ")
+print(sys.getsizeof(size_2))
